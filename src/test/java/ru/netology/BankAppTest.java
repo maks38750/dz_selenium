246d8b8dba79c8e69347f2a9c4c89f6df4a2ae25
Your Name <you@example.com>
2023-07-22 22:17:34 +0500

    updateApp

--- src/test/java/ru/netology/BankAppTest.java
+++ src/test/java/ru/netology/BankAppTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,6 +24,11 @@ class BankAppTest {
 
     @BeforeEach
     void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver = new ChromeDriver();
     }
 
