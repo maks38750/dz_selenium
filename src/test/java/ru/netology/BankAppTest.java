7f04a0ad2d06b7de125a031647d2d16174789847
Your Name <you@example.com>
2023-07-29 12:55:17 +0500

    updete test

--- src/test/java/ru/netology/BankAppTest.java
+++ src/test/java/ru/netology/BankAppTest.java
@@ -1,35 +1,30 @@
 package ru.netology;
 
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BankAppTest {
-
-    WebDriver driver;
+ public class BankAppTest {
+    private WebDriver driver;
 
     @BeforeAll
-    static void setupAll() {
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    void setUp() {
+    void setupTest() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -39,17 +34,14 @@ class BankAppTest {
     }
 
     @Test
-    void positivTest() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Арнольд");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79996521450");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
+    void test0() {
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Арнольд");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79996521450");
+        driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
+        driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-
+        String actual = driver.findElement(By.cssSelector("[data-test-id = order-success]")).getText().trim();
         assertEquals(expected, actual);
-
-
     }
 }
+
